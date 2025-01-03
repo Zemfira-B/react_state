eb72e98b0538ba4bcb2c8bf7ffccfa13657e54cb
fira zem fira <144517701+Zemfira-B@users.noreply.github.com>
2025-01-04 03:26:47 +0500

    server.js

--- my-app/server.js
+++ my-app/server.js
@@ -1,18 +1,3 @@
-//const express = require("express");
-//const cors = require("cors");
-//const app = express();
-//
-//app.use(cors());
-//app.use(express.json());
-//
-//app.get("/message", (req, res) => {
-//  res.json({ message: "Hello, world!" });
-//});
-//
-//app.listen(8000, () => {
-//  console.log(`Server is running on port 8000.`);
-//});
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
